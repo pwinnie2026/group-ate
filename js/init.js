528b7db4746fe9c1ef0b6b131879175df8bf297b
pwinnie2026 <paigewu.business@gmail.com>
2024-07-28 21:55:53 -0700

    adjust nav, map key

--- js/init.js
+++ js/init.js
@@ -1,4 +1,3 @@
-// MODAL
 // Fetch id from HTML (modal)
 var modal = document.getElementById('modal-id');
 
@@ -13,3 +12,13 @@ window.onclick = function (event) {
         modal.style.display = "none"; // Modal display "closes"
     }
 }
+
+// declare variables
+let mapOptions = {'centerLngLat': [-118.444,34.0709],'startingZoomLevel':8}
+
+const map = new maplibregl.Map({
+    container: 'map', // container ID
+    style: 'https://api.maptiler.com/maps/bright/style.json?key=JWrMVIrr3Jz2WGVMeDwh', // Your style URL
+    center: mapOptions.centerLngLat, // Starting position [lng, lat]
+    zoom: mapOptions.startingZoomLevel // Starting zoom level
+});
